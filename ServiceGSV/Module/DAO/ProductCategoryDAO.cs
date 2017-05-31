f782539a4161f41a5f4a820fed443028ff30a506
thatchung <https://github.com/GSVien/Code.git>
2017-05-31 20:56:24 +0700

    aaa
    First commit

---
+++ ServiceGSV/Module/DAO/ProductCategoryDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceGSV.Module.DAO
+{
+    public class ProductCategoryDAO
+    {
+        public List<ProductCategory> GetList()
+        {
+            List<ProductCategory> ass = new List<ProductCategory>();
+            return ass;
+        }
+    }
+}
---
+++ ServiceGSV/Module/DAO/UserInfoDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TFLLib;
+
+namespace ServiceGSV.Module.DAO
+{
+    public class UserInfoDAO
+    {
+        public UserInfo GetById(GiaSinhVienEntities db,long id)
+        {
+            return db.UserInfoes.FirstOrDefault(f => f.Id == id).CloneObject();
+        }
+
+
+    }
+}
