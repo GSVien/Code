f24969120038d2f3b81a9c59c4342bf46d7ad065
thatchung <https://github.com/GSVien/Code.git>
2017-06-05 23:51:04 +0700

    show product
    show product

--- Web Beta/GiaSinhVien/Scripts/_app.js
+++ Web Beta/GiaSinhVien/Scripts/_app.js
@@ -1709,19 +1709,19 @@ giasinhvienApp.directive("img", [
         return {
             restrict: "E",
             link: function (scope, element, attrs) {
-                var errorSrc = getOrDefault($(this).attr("error-src"), "/Content/Image/Background-Item/Meo-gif.gif");
+                //var errorSrc = getOrDefault($(this).attr("error-src"), "/Content/Image/Background-Item/Meo-gif.gif");
 
-                if (attrs.ngSrc === "")
-                    element.attr("src", errorSrc);
+                //if (attrs.ngSrc === "")
+                //    element.attr("src", errorSrc);
 
-                if (element.attr("img-error"))
-                    return;
+                //if (element.attr("img-error"))
+                //    return;
 
-                element.error(function () {
-                    element.off("onerror")
-                        .attr("src", errorSrc)
-                        .attr("img-error", "");
-                });
+                //element.error(function () {
+                //    element.off("onerror")
+                //        .attr("src", errorSrc)
+                //        .attr("img-error", "");
+                //});
             }
         };
     }
