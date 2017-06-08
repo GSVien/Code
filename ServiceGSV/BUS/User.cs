9f7879041dad83170a23bcf25a821679aa5946db
minhtaidev <minhtaidev@users.noreply.github.com>
2017-06-08 15:42:41 +0700

    mã hóa MD5

--- ServiceGSV/BUS/User.cs
+++ ServiceGSV/BUS/User.cs
@@ -14,6 +14,8 @@ namespace ServiceGSV.BUS
     {
         public static DoResult<UserInfo> Bus_DangNhapBangEmail(string _email, string passwork)
         {
+            MaHoaMD5 mahoa = new MaHoaMD5();
+
             return BUSCore.Do<UserInfo>(
                 userId: -1,
                 action: (c) =>
@@ -27,7 +29,7 @@ namespace ServiceGSV.BUS
                     }
                     else // có user=> kiểm tra passwork
                     {
-                        if (userTemp.Passwork != passwork)
+                        if (userTemp.Passwork != mahoa.md5(passwork))
                         {
                             c.SetError("Passwork nhập vào không đúng.Nhập lại", 1);
                             return null;
@@ -43,6 +45,7 @@ namespace ServiceGSV.BUS
 
         public static DoResult<UserInfo> Bus_DangKyUserMoi(string email, string username, string passwork, string photo, string sodienthoai)
         {
+            MaHoaMD5 mahoa = new MaHoaMD5();
             return BUSCore.Do<UserInfo>(
                 userId: -1,
                 action: (c) =>
@@ -57,7 +60,7 @@ namespace ServiceGSV.BUS
                     UserInfo data = new UserInfo();
                     data.Email = email;
                     data.UserName = username;
-                    data.Passwork = passwork;
+                    data.Passwork = mahoa.md5(passwork);
                     data.AvatarPhoto = photo;
                     data.GroupId = 1;
                     data.Phone = sodienthoai;
@@ -70,6 +73,7 @@ namespace ServiceGSV.BUS
         }
         public static DoResult<UserInfo> Bus_ThayDoiThongTinUser(string username, string passwork, string photo, string sodienthoai)
         {
+            MaHoaMD5 mahoa = new MaHoaMD5();
             return BUSCore.Do<UserInfo>(
                 userId: -1,
                 action: (c) =>
@@ -77,7 +81,7 @@ namespace ServiceGSV.BUS
                  
                     UserInfo data = new UserInfo();
                     data.UserName = username;
-                    data.Passwork = passwork;
+                    data.Passwork = mahoa.md5(passwork);
                     data.AvatarPhoto = photo;
                     data.GroupId = 1;
                     data.Phone = sodienthoai;
@@ -88,5 +92,6 @@ namespace ServiceGSV.BUS
                 });
 
         }
+       
     }
 }
---
+++ ServiceGSV/MD5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiceGSV
+{
+
+
+        public class MaHoaMD5
+        {
+            public  byte[] encryptData(string data)
+            {
+                System.Security.Cryptography.MD5CryptoServiceProvider md5Hasher = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] hashedBytes;
+                System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+                hashedBytes = md5Hasher.ComputeHash(encoder.GetBytes(data));
+                return hashedBytes;
+            }
+            public string md5(string data)
+            {
+                return BitConverter.ToString(encryptData(data)).Replace("-", "").ToLower();
+            }
+        }
+    
+}
